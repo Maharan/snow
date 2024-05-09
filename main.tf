6b8ff4eacd610bc97939c3202457bdb4cefb6f9b
Maharan Eiffel Murshed <maharan.murshed@gmail.com>
2024-05-09 22:34:19 +0200

    changes

--- main.tf
+++ main.tf
@@ -9,13 +9,15 @@ terraform {
 
 provider "snowflake" {
   role = "SYSADMIN"
+  account  = var.snowflake_account
+  private_key     = var.snowflake_private_key
 }
 
 resource "snowflake_database" "db" {
   name = "TF_DEMO"
 }
 
-resource "snowflake_warehouse" "warehouse" {
+resource "snowflake_warehouse" "warehouse_a" {
   name           = "TF_DEMO"
   warehouse_size = "xsmall"
   auto_suspend   = 60
