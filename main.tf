23e74dd67921841d347c9067ee158d097b2c5ae2
Maharan Eiffel Murshed <maharan.murshed@gmail.com>
2024-05-14 10:02:22 +0200

    Finish python webscrape and add dbt

--- main.tf
+++ main.tf
@@ -8,17 +8,90 @@ terraform {
 }
 
 provider "snowflake" {
+  user = "tf-snow"
   role = "SYSADMIN"
   account  = var.snowflake_account
-  private_key     = var.snowflake_private_key
+  private_key    = var.snowflake_private_key
+  authenticator = "JWT"
 }
 
 resource "snowflake_database" "db" {
   name = "TF_DEMO"
 }
 
-resource "snowflake_warehouse" "warehouse_a" {
+resource "snowflake_warehouse" "warehouse" {
   name           = "TF_DEMO"
   warehouse_size = "xsmall"
   auto_suspend   = 60
 }
+
+
+resource "snowflake_schema" "RAW" {
+  database   = snowflake_database.db.name
+  name       = "RAW"
+  is_managed = false
+}
+
+resource "snowflake_schema" "STAGE" {
+  database   = snowflake_database.db.name
+  name       = "STAGE"
+  is_managed = false
+}
+
+resource "snowflake_schema" "MART" {
+  database   = snowflake_database.db.name
+  name       = "MART"
+  is_managed = false
+}
+
+# Haven't got this to work out just yet
+/* resource "snowflake_table" "table" {
+  database                    = "db"
+  schema                      = "RAW"
+  name                        = "table"
+
+  column {
+    name     = "city"
+    type     = "text"
+  }
+
+  column {
+    name     = "year"
+    type     = "text"
+  }
+
+  column {
+    name = "type"
+    type = "text"
+  }
+
+  column {
+    name    = "sport"
+    type    = "text"
+  }
+
+  column {
+    name     = "event"
+    type     = "text"
+  }
+
+  column {
+    name     = "medal"
+    type     = "text"
+  }
+
+  column {
+    name     = "country_code"
+    type     = "text"
+  }
+
+  column {
+    name    = "athlete_full_name"
+    type    = "text"
+  }
+  
+  column {
+    name    = "time_added"
+    type    = "text"
+  }
+}*/
--- python_scripts/webscrape.py
+++ python_scripts/webscrape.py
@@ -8,14 +8,21 @@ import random
 import asyncio
 import httpx
 import unidecode
+import datetime
+import os
+from dotenv import load_dotenv, dotenv_values
+from snowflake.connector.pandas_tools import write_pandas
+import snowflake.connector
 
-
+# start timer
 start_time = time.time()
+
+# root url string
 urlroot = 'https://www.olympiandatabase.com'
 
 # create dataframe
-columnnames = ['city', 'year', 'type', 'sport', 'event', 'medal', 'country_code', 'athlete_full_name']
-dummy = [['Askaban', 1234, 'Winter', 'Quidditch', 'Team M', 'Gold', 'GRY', 'Harry Potter'], ['Askaban', 1134, 'Winter', 'Quidditch', 'Team M', 'Gold', 'SLY', 'Tom Riddle']]
+columnnames = ['CITY', 'YR', 'TYPE', 'SPORT', 'EVENT', 'MEDAL', 'COUNTRY_CODE', 'ATHLETE_FULL_NAME', 'TIME_ADDED']
+dummy = [['Askaban', '1234', 'Winter', 'Quidditch', 'Team M', 'Gold', 'GRY', 'Harry Potter', '2024-05-14 01:24:27.978238'], ['Askaban', '1134', 'Winter', 'Quidditch', 'Team M', 'Gold', 'SLY', 'Tom Riddle', '2024-05-14 01:24:26.978238']]
 maintable = pd.DataFrame(dummy, columns = columnnames)
 
 # Async helper function
@@ -31,35 +38,38 @@ def get_team_members(thingy, list_of_values):
     if (thingy.parent is not None) & (thingy.parent.next_sibling is not None) & (thingy.parent.next_sibling.next_sibling is not None):
         if (thingy.parent.next_sibling.next_sibling.td is not None) & (thingy.parent.next_sibling.next_sibling.td.next_sibling is not None):
             if len(str(thingy.parent.next_sibling.next_sibling.td.next_sibling.get_text()))>3:
-                    # Add athlete's name
+                    # Add athlete's name and time
                     list_of_values.append(thingy.parent.next_sibling.next_sibling.td.next_sibling.get_text())
+                    list_of_values.append(str(datetime.datetime.now()))
                     # Add row to table
                     maintable.loc[len(maintable)] = list_of_values
                     # Go recursively until no more team members left
-                    get_team_members(thingy.parent.next_sibling.next_sibling.td, list_of_values[:-1])
+                    get_team_members(thingy.parent.next_sibling.next_sibling.td, list_of_values[:-2])
 
 # At this point, we have ['city', 'year', 'type', 'sport', event]
 def get_people(soup, list_of_values):
     # This will be used to determine whether it is a team event where individual names aren't given
     discriminant = True
-    for thing in soup.css.select("td > img")[1:]:
-        # Create a copy of the value array that can be refreshed with each event
-        list_of_values_copy = list_of_values.copy()
-        # If we have any such match, this means that it isn't a team event without individual names
-        discriminant = discriminant & False
-        # Medal
-        if str(thing)[68:74] == "Gold_m":
-            list_of_values_copy.append("Gold")
-        else:
-            list_of_values_copy.append(str(thing)[68:74])
-        # Country Code
-        list_of_values_copy.append(thing.parent.next_sibling.next_sibling.next_sibling.get_text())
-        # (first) athlete's full name
-        list_of_values_copy.append(thing.parent.next_sibling.get_text())
-        # Add row to table
-        maintable.loc[len(maintable)] = list_of_values_copy
-        # will be used to find more teammates in case it's a team event where individual names are given
-        get_team_members(thing.parent, list_of_values_copy[:-1])
+    for thing in soup.find_all("td", height = "10"):
+        if thing.img is not None:
+            # Create a copy of the value array that can be refreshed with each event
+            list_of_values_copy = list_of_values.copy()
+            # If we have any such match, this means that it isn't a team event without individual names
+            discriminant = discriminant & False
+            # Medal
+            if str(thing)[68:74] == "Gold_m":
+                list_of_values_copy.append("Gold")
+            else:
+                list_of_values_copy.append(str(thing)[68:74])
+            # Country Code
+            list_of_values_copy.append(thing.next_sibling.next_sibling.next_sibling.get_text())
+            # (first) athlete's full name and time
+            list_of_values_copy.append(thing.next_sibling.get_text())
+            list_of_values_copy.append(str(datetime.datetime.now()))
+            # Add row to table
+            maintable.loc[len(maintable)] = list_of_values_copy
+            # will be used to find more teammates in case it's a team event where individual names are given
+            get_team_members(thing, list_of_values_copy[:-2])
     # This is the case where it's a team event where don't have individual names
     if discriminant:
         for thing in soup.find_all("td", height = "15"):
@@ -73,8 +83,9 @@ def get_people(soup, list_of_values):
                     list_of_values_copy.append(str(thing.img)[68:74])
                 # This doesn't exist, so we give NA, which not an actual country code
                 list_of_values_copy.append("NA")
-                # The athlete's name is actually just the name of the country
+                # The athlete's name is actually just the name of the country and then the time
                 list_of_values_copy.append(thing.next_sibling.get_text())
+                list_of_values_copy.append(str(datetime.datetime.now()))
                 # Add row to table
                 maintable.loc[len(maintable)] = list_of_values_copy
 
@@ -89,7 +100,6 @@ def get_events(soup, list_of_values):
             if thing.a is None:
                 if thing.previous_sibling is not None and thing.previous_sibling.previous_sibling is not None and thing.previous_sibling.previous_sibling.previous_sibling is not None and thing.previous_sibling.previous_sibling.previous_sibling.previous_sibling is not None:
                     # Event
-                    #print("Event: ", thing.previous_sibling.previous_sibling.previous_sibling.previous_sibling.get_text())
                     list_of_values_copy.append(unidecode.unidecode(thing.previous_sibling.previous_sibling.previous_sibling.previous_sibling.get_text()))
                     print(list_of_values_copy)
                     # Create next url
@@ -97,13 +107,13 @@ def get_events(soup, list_of_values):
                     # Store next url and values array
                     list_of_events_urls.append([nexturl, list_of_values_copy])
     number_of_events = len(list_of_events_urls)
-    soups = []
+    
     async def helper_events():
         async with httpx.AsyncClient() as client:
 
             tasks = []
             for number in range(0,number_of_events):
-                #print(list_of_events_urls[number][0])
+                
                 url = list_of_events_urls[number][0]
                 tasks.append(asyncio.ensure_future(helper(client, url)))
 
@@ -112,25 +122,16 @@ def get_events(soup, list_of_values):
             print(type(ans))
             for soup in ans:
                 print(type(soup))
-                #print(soup)
+
             for number in range(0,number_of_events):
-                #print(list_of_events_urls[number][0])
                 list_of_events_urls[number].append(ans[number])
-                #tasks[number].cancel()
-    #loop = asyncio.get_running_loop()
-    #loop.create_task(helper_events2())
-    #asyncio.run(helper_events2)
-    #loop = asyncio.get_event_loop()
-    #asyncio.run_coroutine_threadsafe(helper_events2(), loop)
+                
     asyncio.run(helper_events())   
       
     # list_of_urls = [url, ["text", Event], soup]
     for i in list_of_events_urls:
-        # Get athlete, send values url and the values array
+        # Get athlete, send values html code and the values array
         get_people(i[2], i[1])
-        
-        #print("this is i: ", i[0])
-    # return soups
     
 # At this point the list will be [City, Year]
 def get_sports(url, list_of_values):
@@ -147,13 +148,12 @@ def get_sports(url, list_of_values):
         nexturl = urlroot + str(thing.next_sibling.next_sibling.a['href'])
         list_of_sports_urls.append([nexturl, list_of_values_copy])
     number_of_sports = len(list_of_sports_urls)
-    soups = []
+    
     async def helper_sports():
         async with httpx.AsyncClient() as client:
 
             tasks = []
             for number in range(0,number_of_sports):
-                #print(list_of_events_urls[number][0])
                 url = list_of_sports_urls[number][0]
                 tasks.append(asyncio.ensure_future(helper(client, url)))
 
@@ -162,16 +162,10 @@ def get_sports(url, list_of_values):
             print(type(ans))
             for soup in ans:
                 print(type(soup))
-                #print(soup)
+                
             for number in range(0,number_of_sports):
-                #print(list_of_events_urls[number][0])
                 list_of_sports_urls[number].append(ans[number])
-                #tasks[number].cancel()
-    #loop = asyncio.get_running_loop()
-    #loop.create_task(helper_events2())
-    #asyncio.run(helper_events2)
-    #loop = asyncio.get_event_loop()
-    #asyncio.run_coroutine_threadsafe(helper_events2(), loop)
+ 
     asyncio.run(helper_sports())   
       
     # list_of_urls = [url, ["text", Event], soup]
@@ -181,17 +175,30 @@ def get_sports(url, list_of_values):
 
 def get_olympics(url):
     page = requests.get(url)
-    soup = BeautifulSoup(page.text, 'html')
+    soup = BeautifulSoup(page.text, 'html.parser')
+    counter = 0
     for thing in soup.find_all("td", height = "35"):
-        if len(thing.next_sibling.next_sibling.next_sibling.get_text())>0:
-            print("City: ", str(thing.next_sibling.next_sibling.get_text())[0:-5])
-            print("Year: ", str(thing.next_sibling.next_sibling.get_text())[-4:])
+        if len(thing.next_sibling.next_sibling.next_sibling.get_text())>0 and counter < 10:
+            list_of_values = []
+            list_of_values.append(str(thing.next_sibling.next_sibling.get_text())[0:-5])
+            list_of_values.append(str(thing.next_sibling.next_sibling.get_text())[-4:])
             nexturl = urlroot + str(thing.next_sibling.next_sibling.a['href'])
-            get_sports(nexturl)
+            counter += 1
+            get_sports(nexturl, list_of_values)
 
 # Three times faster than in sequence
-get_sports('https://www.olympiandatabase.com/index.php?id=109426&L=1', ['Timbuktu', '0'])
+get_olympics('https://www.olympiandatabase.com/index.php?id=278979&L=1')
 
-#print(objtocheck)
-print(maintable)
+# Open the Snowflake connection
+load_dotenv()
+conn = snowflake.connector.connect(
+    account = os.getenv("SF_ACCOUNT"),
+    user = "eiffel",
+    password = os.getenv("SF_PASSWORD"),
+    role = "ACCOUNTADMIN",  # optional
+    warehouse = "COMPUTE_WH",  # optional
+    database = "TF_DEMO",  # optional
+    schema = "RAW",  # optional 
+)
+success, nchunks, nrows, _ = write_pandas(conn, maintable, 'RAW_FCT_TABLE')
 print("--- %s seconds ---" % (time.time() - start_time))
